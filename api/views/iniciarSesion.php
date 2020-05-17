c4e3af231d7cbf5bc20ba06128c5e71952c738a4
Miguel G <64226714+Migue151999@users.noreply.github.com>
2020-05-16 20:23:57 -0500

    Jala a Base de datos Login

--- api/views/iniciarSesion.php
+++ api/views/iniciarSesion.php
@@ -1,9 +1,7 @@
 <?php
 
 #Recibir usuario y contrase;a por POST
-
 #Responder con un 1 o un 0
-
 #echo 1;
 
 $_POST = json_decode(file_get_contents("php://input"),true);
@@ -13,28 +11,26 @@ $usuario = $datos['usuario'];
 $pass = $datos['pass'];
 
 #conexion
-
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "cursophp";
+$database = "bd_examenweb";
 
 $connection = new mysqli($host, $username, $password, $database);
 if (mysqli_connect_error()) {
 	trigger_error("Error to conecte to MySQL");
 }
 
-if ($usuario && $pass) {
+if (isset($usuario) && isset($pass)) {
 	$query = "Select id from usuarios where Usuario = '$usuario' and Password = '$pass'";
 	$resultado = mysqli_query($connection, $query);
 	$data = array();
 	while ($row = mysqli_fetch_row($resultado))$data = $row;
 	if ($data != null) {
-		echo 1;
+		echo 1; # en caso de que sea correcto da 1
 	}else{
-		echo 0;
+		echo 0; # si no hay nada da 0
 	}
 }else {
 	//echo 'Error';
 }
-echo 1;
